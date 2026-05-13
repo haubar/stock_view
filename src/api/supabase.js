ef4386f25843025b746db9bec43486d8ae138f6f
haubar <haubarclack@gmail.com>
2026-05-13 16:28:53 +0800

    fix k line

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -38,6 +38,7 @@ export async function fetchStockHistory(keyword) {
     `${SUPABASE_URL}/rest/v1/stock_daily` +
     `?stock_id=eq.${encodeURIComponent(stockId)}` +
     `&order=trade_date.desc` +
+    `&limit=2000` +
     `&select=trade_date,stock_id,stock_name,open_price,high_price,low_price,close_price,volume,price_diff,pe_ratio`
 
   const res = await fetch(url, { headers })
